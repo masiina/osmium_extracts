4930181ed3ffccb063e0572ba6d6e324f388e8a9
Jouni Ylärakkola <masiina100@gmail.com>
2024-05-02 13:41:33 +0300

    updates

--- osm_extracts.sh
+++ osm_extracts.sh
@@ -4,6 +4,8 @@
 osmium="/home/masiina/tools/osmium-tool/build/osmium"
 splitter="/home/masiina/tools/splitter/splitter.jar"
 mkgmap="/home/masiina/tools/mkgmap/mkgmap.jar"
+osmosis="/home/masiina/tools/osmosis/bin/osmosis"
+mapsforge="/home/masiina/tools/mapsforge/gradlew"
 PARENT_DIR=$(pwd)
 
 #data inputs
@@ -11,14 +13,13 @@ osm_input="$PARENT_DIR/finland-latest.osm.pbf"
 osm_output=output.osm.pbf
 osm_output1=output1.osm.pbf
 osm_output2=output2.osm.pbf
+poi_file=$PARENT_DIR/POIs.poi
 unpaved_typ="/mnt/g/garmin/typ files/test.typ"
 paved_typ="/mnt/g/garmin/typ files/paved.typ"
-option=
 
 clear
 
-
-# Luodaan työhakemistot
+# Luodaan työhakemisto
 if [ ! -d "data" ]; then
   mkdir -p "data"
 fi
@@ -38,24 +39,44 @@ fi
 while true; do
 echo "1. gravel reitit"
 echo "2. pinnoitetut reitit"
-echo "2. Exit"
+echo "3. päivitä POIt"
+echo "4. Exit"
 read choice
 
   case $choice in
     [1] )
       echo "processing"
-       $osmium tags-filter "$osm_input" w/surface=unpaved,gravel,dirt,earth,fine_gravel w/highway=track w/piste:type -o $osm_output
-       option=$choice
-      break
+      $osmium tags-filter "$osm_input" w/surface=unpaved,gravel,dirt,earth,fine_gravel w/highway=track w/piste:type -o $osm_output
+      java -Xmx6000M -jar "$splitter" $osm_output
+      java -Xmx6000M -jar "$mkgmap" --gmapsupp 6324*.osm.pbf "$unpaved_typ"
+      echo "rename img to gravel"
+      mv gmapsupp.img ../gravel.img
+      cd ..
+      rm -r data/
+      exit 0
       ;;
     [2] )
       echo "processing"
-       $osmium tags-filter "$osm_input" w/surface=asphalt,paved w/highway=secondary,tertiary -o $osm_output1
-       $osmium tags-filter "$osm_output1" w/highway!=motorway,motorway_link,trunk -o $osm_output
-       option=$choice
-      break
+      $osmium tags-filter "$osm_input" w/surface=asphalt,paved w/highway=secondary,tertiary -o $osm_output1
+      $osmium tags-filter "$osm_output1" w/highway!=motorway,motorway_link,trunk -o $osm_output
+      java -Xmx6000M -jar "$splitter" $osm_output
+      java -Xmx6000M -jar "$mkgmap" --gmapsupp 6324*.osm.pbf "$paved_typ"
+      echo "rename img to gravel"
+      mv gmapsupp.img ../paved.img
+      cd ..
+      rm -r data/
+      exit 0
       ;;
     [3] )
+      echo "processing"
+      cd $(dirname $mapsforge)
+      $mapsforge :mapsforge-poi-writer:fatjar
+      mv mapsforge-poi-writer/build/libs/*.jar ../osmosis/bin/plugins/
+      cd $(dirname $osmosis)
+      $osmosis --rb file=$osm_input --poi-writer ways=false file=$poi_file
+      exit 0
+      ;;
+    [4] )
       echo "Exiting..."
       exit 0
       ;;
@@ -65,29 +86,4 @@ read choice
   esac
 done
 
-# buildaus komennot jolla rakennetaan .img tiedosto
-java -Xmx6000M -jar "$splitter" $osm_output
-
-# nimeä tiedosto valinnan mukaan
-case $option in
-  [1] )
-    java -Xmx6000M -jar "$mkgmap" --gmapsupp 6324*.osm.pbf "$unpaved_typ"
-    echo "rename img to gravel"
-    mv gmapsupp.img ../gravel.img
-    ;;
-  [2] )
-    java -Xmx6000M -jar "$mkgmap" --gmapsupp 6324*.osm.pbf "$paved_typ"
-    echo "rename img to gravel"
-    mv gmapsupp.img ../paved.img
-    ;;
-  * )
-    echo "Nothing to do, done"
-    ;;
-esac
-
-# työtiedostojen poisto
-cd ..
-rm -r data/
-
-echo "work done"
 exit 0
